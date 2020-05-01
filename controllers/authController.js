7d1121b37f2f1c6634fed426ae41d71b7e8b5418
ngochoa <nguyenngochoa3979@gmail.com>
2020-05-01 15:57:30 +0700

    Building the user account page

--- controllers/authController.js
+++ controllers/authController.js
@@ -106,6 +106,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     }
     //GRANT ACCESS TO PROTECT ROUTE
     req.user = currentUser;
+    res.locals.user = currentUser;
     next();
 });
 
