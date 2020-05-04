696c8f4b7508209a771162321b213370772a4c35
ngochoa <nguyenngochoa3979@gmail.com>
2020-05-04 22:36:23 +0700

    Resizing images

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,22 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./../controllers/handlerFactory');
 const AppError = require('./../utils/appErorr');
 
-const multerStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/img/users');
-    },
-    filename: (req, file, cb) => {
-        // user-767676abc76dba-3232232323.jpeg 1 img with 1 filename
-        //      user-id        timestamp
-        const ext = file.mimetype.split('/')[1];
-        cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-    }
-});
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/img/users');
+//     },
+//     filename: (req, file, cb) => {
+//         // user-767676abc76dba-3232232323.jpeg 1 img with 1 filename
+//         //      user-id        timestamp
+//         const ext = file.mimetype.split('/')[1];
+//         cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//     }
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
@@ -30,6 +32,19 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+    if (!req.file) return next();
+
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+    sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/users/${req.file.filename}`);
+    next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -54,6 +69,7 @@ exports.getMe = (req, res, next) => {
     req.params.id = req.user.id;
     next();
 };
+
 exports.updateMe = catchAsync(async (req, res, next) => {
     // console.log(req.file);
     // console.log(req.body);
