348dadc398d18bc7e61f9396b9351922756295e6
ngochoa <nguyenngochoa3979@gmail.com>
2020-05-08 21:25:05 +0700

    Using Sendgrid for real emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,12 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
     //log user in as soon as he signed up
 
     const url = `${req.protocol}://${req.get('host')}/me`;
